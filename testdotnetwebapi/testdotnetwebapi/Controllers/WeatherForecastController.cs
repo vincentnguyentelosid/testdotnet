c6d2c883f980b299f862b181baff702e85725f5f
Vincent Nguyen <Vincent.Nguyen@telos.com>
2023-04-10 21:51:24 -0400

    second commit

--- testdotnetwebapi/testdotnetwebapi/Controllers/WeatherForecastController.cs
+++ testdotnetwebapi/testdotnetwebapi/Controllers/WeatherForecastController.cs
@@ -25,7 +25,8 @@ namespace testdotnetwebapi.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                Notes = "Hello World"
             })
             .ToArray();
         }
--- testdotnetwebapi/testdotnetwebapi/WeatherForecast.cs
+++ testdotnetwebapi/testdotnetwebapi/WeatherForecast.cs
@@ -9,5 +9,6 @@ namespace testdotnetwebapi
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public string? Notes { get; set; }
     }
 }
